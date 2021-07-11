fbec01f539c2ef7e2d2822e619f109c9c948ed8b
Develeopard <frontendwebdeveleopard@gmail.com>
2021-07-11 22:13:34 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAdum4VIQ-mmetDG9JxeZ5m6zB3dGXI8WQ",
+    authDomain: "vue-ath-fb.firebaseapp.com",
+    projectId: "vue-ath-fb",
+    storageBucket: "vue-ath-fb.appspot.com",
+    messagingSenderId: "695520185172",
+    appId: "1:695520185172:web:5f7a438c87d51aaec596c1"
+  };
+
+  firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount('#app')
